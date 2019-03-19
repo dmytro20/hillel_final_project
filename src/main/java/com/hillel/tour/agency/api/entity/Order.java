4164c2150237fcd24073cdc2cbe0ec5e41f0cf90
dmytrobiletskyi <dmytrobiletskyi@sophos.com>
2019-03-19 14:52:31 +0200

    Fix get all orders

--- src/main/java/com/hillel/tour/agency/api/entity/Order.java
+++ src/main/java/com/hillel/tour/agency/api/entity/Order.java
@@ -2,21 +2,25 @@ package com.hillel.tour.agency.api.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Table(name = "orders")
-public class Order {
+public class Order
+{
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "guid", unique = true)
+    private UUID guid;
 
     @Column(name = "status")
     private String status;
@@ -24,74 +28,103 @@ public class Order {
     @Column(name = "date")
     private String date;
 
-    @Column(name = "tour_id")
-    private Integer tour_id;
+    @Column(name = "id_tour")
+    private Integer tourId;
 
-    @Column(name = "user_id")
-    private Integer user_id;
+    @Column(name = "id_user")
+    private Integer userId;
 
-    public UUID getId() {
+    public Integer getId()
+    {
         return id;
     }
 
-    public void setId(UUID id) {
+    public void setId(Integer id)
+    {
         this.id = id;
     }
 
-    public String getStatus() {
+    public UUID  getGuid()
+    {
+        return guid;
+    }
+
+    public void setGuid(UUID guid)
+    {
+        this.guid = guid;
+    }
+
+    public String getStatus()
+    {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(String status)
+    {
         this.status = status;
     }
 
-    public String getDate() {
+    public String getDate()
+    {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate(String date)
+    {
         this.date = date;
     }
 
-    public Integer getTour_id() {
-        return tour_id;
+    public Integer getTourId()
+    {
+        return tourId;
     }
 
-    public Integer getUser_id() {
-        return user_id;
+    public void setTourId(Integer tourId)
+    {
+        this.tourId = tourId;
     }
 
-    public void setTour_id(Integer tour_id) {
-        this.tour_id = tour_id;
+    public Integer getUserId()
+    {
+        return userId;
     }
 
-    public void setUser_id(Integer user_id) {
-        this.user_id = user_id;
+    public void setUserId(Integer userId)
+    {
+        this.userId = userId;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+    public boolean equals(Object o)
+    {
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
         Order order = (Order) o;
         return Objects.equals(id, order.id) &&
-                Objects.equals(status, order.status) &&
-                Objects.equals(date, order.date);
+               Objects.equals(guid, order.guid) &&
+               Objects.equals(status, order.status) &&
+               Objects.equals(date, order.date) &&
+               Objects.equals(tourId, order.tourId) &&
+               Objects.equals(userId, order.userId);
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(id, status, date);
+    public int hashCode()
+    {
+        return Objects.hash(id, guid, status, date, tourId, userId);
     }
 
     @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", status='" + status + '\'' +
-                ", date='" + date + '\'' +
-                '}';
+    public String toString()
+    {
+        final StringBuilder sb = new StringBuilder("Order{");
+        sb.append("id=").append(id);
+        sb.append(", guid='").append(guid).append('\'');
+        sb.append(", status='").append(status).append('\'');
+        sb.append(", date='").append(date).append('\'');
+        sb.append(", tourId=").append(tourId);
+        sb.append(", userId=").append(userId);
+        sb.append('}');
+        return sb.toString();
     }
-
 }
--- src/main/java/com/hillel/tour/agency/api/service/impl/OrderServiceImpl.java
+++ src/main/java/com/hillel/tour/agency/api/service/impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import com.hillel.tour.agency.api.repository.postgre.OrdersPostgreRepository;
 import com.hillel.tour.agency.api.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
     }
